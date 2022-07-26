ba0919c9bf1673a581db90505649e3e2c9bc8aa7
Serega71RUS <mister.Cerz@yandex.ru>
2022-07-26 11:58:29 +0300

    Exercise 2 Complete

---
+++ Allfiles/Mod01/Labfiles/02_CakeStoreApi_begin/CakeStoreApi/Controllers/CakeStoreApiController.cs
@@ -0,0 +1,33 @@
+﻿using CakeStoreApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CakeStoreApi.Controllers
+{
+    public class CakeStoreApiController : Controller
+    {
+        private IData _data;
+
+        public CakeStoreApiController(IData data)
+        {
+            _data = data;
+        }
+
+        [HttpGet("/api/CakeStore")]
+        public ActionResult<List<CakeStore>> GetAll()
+        {
+            return _data.CakesInitializeData();
+        }
+
+        [HttpGet("/api/CakeStore/{id}", Name = "GetCake")]
+        public ActionResult<CakeStore> GetById(int? id)
+        {
+            var item = _data.GetCakeById(id);
+
+            if (item == null)
+                return NotFound();
+
+            return new ObjectResult(item);
+        }
+    }
+}
